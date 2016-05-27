bee3ba4d0579ed256deb7734b012f3b4d13ecd8f
Moritz Bauer <mb@sysvyz.org>
2016-05-27 22:18:35 +0200

    update

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'dbname' => 'd02217de',
-    'user' => 'd02217de',
-    'password' => 'BpvgT7DPTQdL9xMT',
-    'host' => 'dd19200.kasserver.com',
-    'driver' => 'pdo_mysql',
-];
